90e98b111ba44f83b0cf71f86dad56f445a44d3b
monax-owo <zprl.dev@gmail.com>
2026-01-16 17:03:40 +0900

    up

--- gen_loot_table.py
+++ gen_loot_table.py
@@ -1,24 +1,28 @@
 from typing import Optional
 
 import json
-import os
 
-OUTDIR = "data/dun/loot_table/items"
+LT_DIR = "data/dun/loot_table"
+ALL_LT_FILE = "all_items.json"
 
 
 def main():
     with open("items.json") as f:
         table = json.load(f)
 
-    if not os.path.exists(OUTDIR) and not os.path.isdir(OUTDIR):
-        os.makedirs(OUTDIR)
+    all_lt_data = {"pools": []}
 
     for v in table["items"]:
         # gen lt
         loot_table_str = gen_lt(v["id"], v["tier"], v["price"], v["name"], v["lore"])
         formatted_id = str(v["id"]).removeprefix("minecraft:")
-        with open(f"{OUTDIR}/{formatted_id}.json", mode="w") as f:
+        with open(f"{LT_DIR}/items/{formatted_id}.json", mode="w") as f:
             json.dump(loot_table_str, f, indent=2, ensure_ascii=False)
+
+        all_lt_data["pools"].append({"rolls": 1, "entries": [{"type": "loot_table", "value": f"dun:items/{formatted_id}"}]})
+
+    with open(f"{LT_DIR}/{ALL_LT_FILE}", mode="w") as f:
+        json.dump(all_lt_data, f, indent=2, ensure_ascii=False)
 
 
 def gen_lt(id, tier: int, price: int, name: str, lore: object) -> Optional[dict[str]]:
@@ -57,7 +61,7 @@ def gen_lt(id, tier: int, price: int, name: str, lore: object) -> Optional[dict[
     if name is None or name == "":
         print("name is None")
     else:
-        data["pools"][0]["entries"][0]["functions"].append({"function": "minecraft:set_name", "name": name})
+        data["pools"][0]["entries"][0]["functions"].append({"function": "minecraft:set_name", "name": {"text": name, "italic": False}})
 
     if lore is None or lore == "":
         print("lore is None")
